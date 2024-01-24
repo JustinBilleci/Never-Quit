1b4159fd1546bb9b33a80a4e3b1276f3c9b4c750
Justin Billeci <justinbilleci15@gmail.com>
2024-01-23 19:59:10 -0500

    starting it up
    starting my workout app

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+    <div>
+        <p>Here we...</p>
+        <h1>NEVER<span>QUIT</span></h1>
+        <p>Being able to keep going when you want to quit is a skill not many have, our goal here at <span>NeverQuit</span> is to hold you accountable for all of your workout goals.</p>
+    </div>
+    )
+}
