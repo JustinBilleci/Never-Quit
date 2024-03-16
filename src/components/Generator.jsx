6f08881002fc68ef99988b1b62ed41d5851f9413
Justin Billeci <justinbilleci15@gmail.com>
2024-03-15 22:01:09 -0400

    Some Code Progress

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import SectionWrapper from "./SectionWrapper";
 
 export default function Generator() {
-    return (
-    <div>Generator</div>
+    return ( // unlike in our App.jsx the Generator.jsx must have an opening and closing component
+    <SectionWrapper header={"generate your workout"} title={['It\'s', 'Grind', 'Time']}>
+        hello world
+    </SectionWrapper >
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react'
 
 export default function Hero() {
-    return (
-    <div>
-        <p>Here we...</p>
-        <h1>NEVER<span>QUIT</span></h1>
-        <p>Being able to keep going when you want to quit is a skill not many have, our goal here at <span>NeverQuit</span> is to hold you accountable for all of your workout goals.</p>
+    return (          // w Tailwind CSS you can change the CSS for an element directly by the className, to do custom amount of pixels in Tailwind you must use []
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>  
+        <div className='flex flex-col gap-4'>
+        <p>Here, we...</p>
+        <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>NEVER<span className='text-blue-400'>QUIT</span></h1>
+        </div>
+        <p className='text-sm md:text-base font-light'>Being able to keep going when you want to give up is a skill not many have, our goal here at <span className='text-blue-400 font-medium'>NeverQuit</span> is to hold you accountable for all of your workout goals.</p>
+        <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'> 
+            <p>Accept & Begin</p>
+        </button>
     </div>
-    )
+    ) // duration on button makes it incredibly smooth
 }
